9a408b638e6efdccbd9530c7ce2384a37c7419ea
UrvashiBhadaniya <145823903+UrvashiBhadaniya@users.noreply.github.com>
2023-09-23 10:20:45 +0530

    Add files via upload

---
+++ Output veriables.py
@@ -0,0 +1,28 @@
+a = 5
+b = 10
+print(a + b)
+
+p = 'Urvashi '
+q = 'Sahil '
+r = 'Bhadaniya'
+print(p+q+r)
+
+x = '1'
+y = 'Veera'
+print(x,y)
+
+m = 'Rushikesh'
+n = 'Shivam'
+print(m)
+print(n)
+
+                                                    #variables 
+Students = ['Heer','Devanshi','Swara','Dhyan','Jaydev']
+a, b, c, d, e = Students
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
